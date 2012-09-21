0d50b7ecb7b6e313f8f81846c18004b1abe46898
Yuriy Dybskiy <yuriy@dybskiy.com>
2012-09-20 19:46:08 -0700

    Add some emoticons

--- client/main.js
+++ client/main.js
@@ -13,8 +13,8 @@ Template.worldhack.meta = function () {
 Template.item.events = {
     'click': function () {
 
-      Meteor.http.post("http://worldhack.meteor.com/me/brandwarsgraph:adore",
-        {brand: 'http://worldhack.meteor.com/Nikon.html'},
+      Meteor.http.post("http://graph.facebook.com/me/brandwarsgraph:adore",
+        {brand: 'http://worldhack.meteor.com/Nikon.html', access_token: 'AAACzYNkXPSoBAJv6fzkZCW337wxVUdHMldiq6g3lbG4txbobgbAZCGI3ZCDfCvpZAXJZCjyhO2cSDh0JM8hhJze1KPX3guQyQZCWg74gJbSAZDZD' },
         function (error, result) {
                    if (result.statusCode === 200) {
                      console.log("You failed!");
