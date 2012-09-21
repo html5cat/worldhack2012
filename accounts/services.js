20bed41033eab4d67cc0b5fe2f7b21701546eb41
Yuriy Dybskiy <yuriy@dybskiy.com>
2012-09-20 23:04:31 -0700

    Fix sign-in with facebook

--- accounts/services.js
+++ accounts/services.js
@@ -1,5 +1,5 @@
 // Modify and uncomment the following lines to configure login services.
 // Also see accounts/server/secrets.js
 
-Meteor.accounts.facebook.config('197228540411178', 'http://worldhack.meteor.com/');
+Meteor.accounts.facebook.config('197228540411178', 'http://worldhack.meteor.com');
 // Meteor.accounts.google.config('987846107089.apps.googleusercontent.com', 'http://auth-todos.meteor.com');
