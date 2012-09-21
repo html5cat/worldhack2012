78ddf93c03aa634e7cf12534ce9c48dc7bb19e69
Yuriy Dybskiy <yuriy@dybskiy.com>
2012-09-20 17:08:45 -0700

    Gauges not working. ARGH

--- client/main.js
+++ client/main.js
@@ -10,10 +10,33 @@ Template.worldhack.meta = function () {
     return Meta.find();
 };
 
-// function getNikonCanonIndex() {
 
-//   console.log(Meta.find( {"nikonCount"} ));
+Meteor.startup( function() {
 
-// }
+  // function getNikonCanonIndex() {
 
-// getNikonCanonIndex();
+  //   console.log(Meta.find( {"nikonCount"} ));
+
+  // }
+
+  // getNikonCanonIndex();
+
+  // var opts = {
+  //   lines: 12, // The number of lines to draw
+  //   angle: 0.15, // The length of each line
+  //   lineWidth: 0.44, // The line thickness
+  //   pointer: {
+  //     length: 0.9, // The radius of the inner circle
+  //     strokeWidth: 0.035 // The rotation offset
+  //   },
+  //   colorStart: '#6FADCF',   // Colors
+  //   colorStop: '#8FC0DA',    // just experiment with them
+  //   strokeColor: '#E0E0E0'   // to see which ones work best for you
+  // };
+  // var target = document.getElementById('NikonGauge'); // your canvas element
+  // var gauge = new Gauge(target).setOptions(opts); // create sexy gauge!
+  // gauge.value = 1250; // set actual value
+  // gauge.maxValue = 3000; // set max gauge value
+
+});
+
