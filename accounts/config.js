25118341e81da8848640dd7269391c2a62df3212
Yuriy Dybskiy <yuriy@dybskiy.com>
2012-10-24 13:30:38 -0700

    Update to Meteor 0.5.0 and temporary remove links for iPhone app

--- accounts/config.js
+++ accounts/config.js
@@ -1,5 +1,5 @@
-Meteor.accounts.config({
-  requireEmail: false,
-  requireUsername: false,
-  validateEmails: true
-});
+// Accounts.config({
+//   requireEmail: false,
+//   requireUsername: false,
+//   validateEmails: true
+// });
